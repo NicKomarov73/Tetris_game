369da61fc39da1a72b8878a9993a26f1c6b678c8
NicKomarov73 <komarov@lic145.kiev.ua>
2020-12-14 03:42:13 +0200

    add figure's movement

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,40 @@
+import Figure
+
+
 class Tetris():
+    figure = None
+
     def __init__(self, height, width, tile):
         self.height = height
         self.width = width
         self.tile = tile
+
+    def new_figure(self):
+        self.figure = Figure.Figure(3, 0)
+
+    def intersect(self):
+        intersection = False
+        fig_im = self.figure.image()
+        for i in range(4):
+            if self.figure.x + fig_im[i] % 4 < 0 or self.figure.x + fig_im[i] % 4 > self.width - 1:
+                intersection = True
+
+        return intersection
+
+    def go_side(self, dx):
+        temp = self.figure.x
+        self.figure.x += dx
+        if self.intersect():
+            self.figure.x = temp
+
+    def move_down(self):
+        temp = self.figure.y
+        self.figure.y += 1
+        if self.intersect():
+            self.figure.y = temp
+
+    def rotate(self):
+        temp = self.figure.rotation
+        self.figure.rotate()
+        if self.intersect():
+            self.figure.rotation = temp
--- main.py
+++ main.py
@@ -6,10 +6,9 @@ black = (0, 0, 0)
 green = (0, 255, 0)
 gray = (40, 40, 40)
 
-
 # creating game window
 game = Tetris.Tetris(20, 10, 45)
-SIZE = (game.width*game.tile, game.height*game.tile)
+SIZE = (game.width * game.tile, game.height * game.tile)
 FPS = 60
 
 pg.init()
@@ -17,21 +16,57 @@ screen = pg.display.set_mode(SIZE)
 pg.display.set_caption("Tetris")
 clock = pg.time.Clock()
 
+speed_counter, speed, speed_limit, super_speed_counter = 0, 400, 2000, 0
 
 run = True
 while run:
-    clock.tick(FPS)
+    if game.figure == None:
+        game.new_figure()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                game.go_side(-1)
+            if event.key == pg.K_RIGHT:
+                game.go_side(1)
+            if event.key == pg.K_UP:
+                game.rotate()
+            if event.key == pg.K_DOWN:
+                speed_limit = 100
 
     screen.fill(black)
 
-    grid = [pg.Rect(x*game.tile, y*game.tile, game.tile, game.tile) for x in range(game.width) for y in range(game.height)]
-
+    # draw field
+    grid = [pg.Rect(x * game.tile, y * game.tile, game.tile, game.tile) for x in range(game.width) for y in range(game.height)]
     [pg.draw.rect(screen, gray, i, 1) for i in grid]
 
-    pg.display.flip()
+    # draw figure
+    fig = game.figure
+    fig_im = fig.image()
+    fig_rec = pg.Rect(0, 0, game.tile, game.tile)
+    for i in range(4):
+        fig_rec.x = (fig.x + fig_im[i] % 4) * game.tile
+        fig_rec.y = (fig.y + fig_im[i] // 4) * game.tile
+        pg.draw.rect(screen, green, fig_rec)
 
+    # move down
+    speed_counter += speed
+    if speed_limit == 100:
+        super_speed_counter += 1
+    if speed_counter > speed_limit:
+        speed_counter = 0
+        if super_speed_counter > 6:
+            speed_limit = 2000
+            super_speed_counter = 0
+        game.move_down()
+
+        for i in range(4):
+            if fig.y + fig_im[i] // 4 > game.height - 1:
+                game.new_figure()
+
+    pg.display.flip()
+    clock.tick(FPS)
 
 pg.quit()
